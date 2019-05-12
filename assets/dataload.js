cf21c4b35f6c9f4ff12d56c8cb5ac159193c52b9
bartektenDev <bartek8991@live.com>
2019-05-12 14:31:34 -0700

    Lots of added features

--- assets/dataload.js
+++ assets/dataload.js
@@ -3,7 +3,7 @@
 function setup() {
 	readConfigFile();
 	dataPing();
-	downloadWeatherData();
+	readWeatherData()
 }
 
 function readConfigFile() {
@@ -107,8 +107,6 @@ function readData() {
 			var devicesConnected = testRE[1];
 			document.getElementById("numOfDevices").innerHTML = devicesConnected;
 		}
-
-
 	}
 }
 
@@ -181,37 +179,90 @@ function dataPing() {
 			document.getElementById("sharedDisplayIP").style.backgroundColor = "#f05d58";
 		}
 	};
+	try {
+		http.send(null);
+	} catch (exception) {
+		// this is expected
+		alert('failed');
+	}
 }
 
-function downloadWeatherData()	{
-	var downloadLink = "https://api.darksky.net/forecast/11b0a87a73edc45033c6dd08b105fe8a/";
-	var strlat = "42.3601";
-	var strlong = "-71.0589";
-	var fullRequestString = downloadLink + strlat + "," + strlong;
-	
-	parseURLParams(fullRequestString);
-	var urlString = "http://www.foo.com/bar?a=a+a&b%20b=b&c=1&c=2&d#hash";
-    urlParams = parseURLParams(urlString);
-	alert(urlParams);
+function readWeatherData()	{
+	var file = "./assets/currentweather.json";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				var allText = rawFile.responseText;
+				
+				//grab the weather temp 
+				var firstvariable = 'temperature":';
+				var secondvariable = ',"';
+				var regExString = new RegExp("(?:" + firstvariable + ")(.*?)(?:" + secondvariable + ")", "ig");
+				var storeRawData = allText;
+				var testRE = regExString.exec(storeRawData);
+
+				if (testRE && testRE.length > 1) {
+					var temp = testRE[1];
+					document.getElementById("readTEMP").innerHTML = temp;
+				}
+				
+				//grab the weather visual idea of the weather
+				var firstvariable2 = 'icon":"';
+				var secondvariable2 = '","';
+				var regExString2 = new RegExp("(?:" + firstvariable2 + ")(.*?)(?:" + secondvariable2 + ")", "ig");
+				var storeRawData2 = allText;
+				var testRE2 = regExString2.exec(storeRawData2);
+
+				if (testRE2 && testRE2.length > 1) {
+					var visual = testRE2[1];
+					document.getElementById("readVISUAL").innerHTML = visual;
+				}
+				
+				//grab the weather visibility outside
+				var firstvariable3 = 'visibility":';
+				var secondvariable3 = ',"';
+				var regExString3 = new RegExp("(?:" + firstvariable3 + ")(.*?)(?:" + secondvariable3 + ")", "ig");
+				var storeRawData3 = allText;
+				var testRE3 = regExString3.exec(storeRawData3);
+
+				if (testRE3 && testRE3.length > 1) {
+					var visibility = testRE3[1];
+					document.getElementById("readVISIBILITY").innerHTML = visibility;
+				}
+				
+				//grab the weather wind speed
+				var firstvariable4 = 'windSpeed":';
+				var secondvariable4 = ',"';
+				var regExString4 = new RegExp("(?:" + firstvariable4 + ")(.*?)(?:" + secondvariable4 + ")", "ig");
+				var storeRawData4 = allText;
+				var testRE4 = regExString4.exec(storeRawData4);
+
+				if (testRE4 && testRE4.length > 1) {
+					var speed = testRE4[1];
+					document.getElementById("readWINDSPEED").innerHTML = speed;
+				}
+			}
+		}
+	};
+	rawFile.send(null);
 }
 
-function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
-
-    if (query === url || query === "") return;
-
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=", 2);
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
-
-        if (!parms.hasOwnProperty(n)) parms[n] = [];
-        parms[n].push(nv.length === 2 ? v : null);
-    }
-    return parms;
+function startUptime()	{
+	var counter = 0;//set this to what ever you want the start # to be
+	countUP ();//call the function once	
+	function countUP () {
+		counter++;//increment the counter by 1
+		setTimeout ( "countUP()", 1000 );//runs itsself after 1000 miliseconds
+		//console.log(counter);uncomment to can see it in action, only with firebug
+	}
 }
 
+var counter = 0;//set this to what ever you want the start # to be
+	countUP ();//call the function once	
+	function countUP () {
+		counter++;//increment the counter by 1
+		setTimeout ( "countUP()", 1000 );//runs itsself after 1000 miliseconds
+		//console.log(counter);uncomment to can see it in action, only with firebug
+	}
---
+++ assets/getWeatherData.py
@@ -0,0 +1,46 @@
+import urllib2
+import os
+import sys
+import time
+import subprocess as sp
+#Copyright and all rights all reserved to Bart Tarasewicz
+
+def cleanSlate():
+    print('checking data...')
+    exists = os.path.isfile('./currentweather.json')
+    if exists:
+        print('cleaning data...')
+        os.remove("currentweather.json")
+        getWeather()
+    else:
+        print('no cache found...')
+        getWeather()
+    
+def getWeather():
+    latitude = "41.9747306"
+    longitude = ",-88.0698165"
+    fullRequestString = "https://api.darksky.net/forecast/11b0a87a73edc45033c6dd08b105fe8a/%s%s" % (latitude,longitude)
+    response = urllib2.urlopen(fullRequestString)
+    html = response.read()
+    print('reading data...')
+    print(html)
+    print('successfully read data!')
+    f=open("currentweather.json", "a+")
+    f.write(html)     
+    f.close()
+    timeOut();
+
+def timeOut():
+    #every 20 minutes get new weather data in seconds
+    print('Starting timer in 5 seconds..')
+    time.sleep(5)
+    print('Waiting 20 minutes till next data grab')
+    for i in xrange(1200,0,-1):
+        sys.stdout.write('Waiting ' +str(i)+' seconds till next weather grab')
+        sys.stdout.flush()
+        time.sleep(1)
+        sp.call('cls',shell=True)
+    print('Timer finished! Getting more data again.')
+    cleanSlate()
+
+cleanSlate()
